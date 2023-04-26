c52775a59ae569555fd00b8bfd24a3c271c1a058
LaisAlba <116845539+LaisAlba@users.noreply.github.com>
2023-04-26 12:50:32 -0300

    adicao dos filtros por idade e por idiomas

--- ProjetoGestaoRH/src/main/java/br/com/taking/ProjetoGestaoRH/controller/CandidatoController.java
+++ ProjetoGestaoRH/src/main/java/br/com/taking/ProjetoGestaoRH/controller/CandidatoController.java
@@ -1,7 +1,9 @@
 package br.com.taking.ProjetoGestaoRH.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import br.com.taking.ProjetoGestaoRH.repository.CandidatoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,89 +18,134 @@ import br.com.taking.ProjetoGestaoRH.service.CandidatoService;
 
 @RestController
 public class CandidatoController {
-	
-	@Autowired
-	CandidatoService candidatoService;
-	
-	@RequestMapping(value= "/candidato", method = RequestMethod.GET)
-	public ResponseEntity<List<Candidato>> GetALL(){
-		
-		List<Candidato> resultado = candidatoService.listarTodos();
-		return new ResponseEntity<List<Candidato>> (resultado, HttpStatus.OK);
-		
-	}
 
-	
-	
-	//INCLUIR	
-		@RequestMapping(value= "/candidato", method = RequestMethod.POST) 
-		public Candidato Post(@RequestBody Candidato candidato) {
-			
-			return candidatoService.incluir(candidato);
-		}
+    @Autowired
+    CandidatoService candidatoService;
 
-		
-		
-		
-		
-		//OBTER POR ID
-		@RequestMapping(value = "/candidato/{id}", method = RequestMethod.GET)
-		public ResponseEntity<Candidato> GetById(@PathVariable(value ="id") int id){
-			 Candidato candidato = candidatoService.obterPorId(id);
-			 if(candidato != null) {
-				 return new ResponseEntity<Candidato>(candidato, HttpStatus.OK );
-				}else {
-					return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-				
-			 }
-		}
-		
-		// ATUALIZAR
-		@RequestMapping(value= "/candidato/{id}", method = RequestMethod.PUT)
-		public ResponseEntity<Candidato> Put(@PathVariable (value="id") int id,@RequestBody Candidato candidato){
-		Candidato candidatoAtualizado = candidatoService.atualizar(id, candidato);
-		
-		    if (candidatoAtualizado != null) {
-	             return new ResponseEntity<Candidato>(candidatoAtualizado, HttpStatus.OK);
-	    	
-	         }else{
-	    	    return new ResponseEntity<Candidato>( HttpStatus.NOT_FOUND);
-		}
-		}
-		//DELETAR
-		  @RequestMapping(value = "/candidato/{id}", method = RequestMethod.DELETE)
-			public ResponseEntity<Object> Delete(@PathVariable(value = "id") int id){
-				Candidato cliente = candidatoService.obterPorId(id);
-				if(cliente != null) {
-					candidatoService.deletar(id);
-					return new ResponseEntity<>(HttpStatus.OK);
-				} else {
-					return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-				}	
-			}
-		  
-		 
-		  //FILTRO Documento
-		  
-		  @RequestMapping(value = "/candidato/filter{identificationDocument}", method = RequestMethod.POST)
-		  public ResponseEntity<List<Candidato>> FindByIdentification(@PathVariable (value= "identificationDocument") String identification){
-			  
-			List<Candidato> candidato =  candidatoService.findByIdentificationDocument(identification);
-			if(candidato != null) {
-				return new ResponseEntity<>(candidato, HttpStatus.OK);
-			} else {
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-			
-			}
-			
-			
-		  }
-		  
-		  
-	
-		    		  
-		  		
-		  		
-		  		
-		  	}
+    @Autowired
+    private CandidatoRepository candidatoRepository;
+
+    @RequestMapping(value = "/candidato", method = RequestMethod.GET)
+    public ResponseEntity<List<Candidato>> GetALL() {
+        List<Candidato> resultado = candidatoService.listarTodos();
+        return new ResponseEntity<List<Candidato>>(resultado, HttpStatus.OK);
+    }
+
+    //INCLUIR
+    @RequestMapping(value = "/candidato", method = RequestMethod.POST)
+    public Candidato Post(@RequestBody Candidato candidato) {
+        return candidatoService.incluir(candidato);
+    }
+
+    //OBTER POR ID
+    @RequestMapping(value = "/candidato/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Candidato> GetById(@PathVariable(value = "id") int id) {
+        Candidato candidato = candidatoService.obterPorId(id);
+        if (candidato != null) {
+            return new ResponseEntity<Candidato>(candidato, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
+    }
+
+    // ATUALIZAR
+    @RequestMapping(value = "/candidato/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Candidato> Put(@PathVariable(value = "id") int id, @RequestBody Candidato candidato) {
+        Candidato candidatoAtualizado = candidatoService.atualizar(id, candidato);
+
+        if (candidatoAtualizado != null) {
+            return new ResponseEntity<Candidato>(candidatoAtualizado, HttpStatus.OK);
+
+        } else {
+            return new ResponseEntity<Candidato>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //DELETAR
+    @RequestMapping(value = "/candidato/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> Delete(@PathVariable(value = "id") int id) {
+        Candidato cliente = candidatoService.obterPorId(id);
+        if (cliente != null) {
+            candidatoService.deletar(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    //FILTRO Documento
+    @RequestMapping(value = "/candidato/filter{identificationDocument}", method = RequestMethod.POST)
+    public ResponseEntity<List<Candidato>> FindByIdentification(@PathVariable(value = "identificationDocument") String identification) {
+        List<Candidato> candidato = candidatoService.findByIdentificationDocument(identification);
+        if (candidato != null) {
+            return new ResponseEntity<>(candidato, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /**
+     * Filtrar o candidato enviando a idade especifica no header
+     * @param age idade escolhida pelo usuario
+     * @return
+     */
+    @RequestMapping(value = "/candidato/filter/age/{age}", method = RequestMethod.GET)
+    public ResponseEntity<List<Candidato>> FilterAge(@PathVariable(value = "age") int age) {
+        List<Candidato> candidato = candidatoRepository.filterDate(age);
+        if (candidato.size() > 0)
+            return new ResponseEntity<>(candidato, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Filtrar o candidato enviando duas idades no header
+     * @param age1 primeira idade escolhida pelo usuario
+     * @param age2 segunda idade escolhida pelo usuario
+     * @return
+     */
+    @RequestMapping(value = "/candidato/filter/ages/{age1}/{age2}", method = RequestMethod.GET)
+    public ResponseEntity<List<Candidato>> FilterAge(@PathVariable(value = "age1") int age1, @PathVariable(value = "age2") int age2) {
+        List<Candidato> candidato = candidatoRepository.filterAges(age1, age2);
+        if (candidato.size() > 0)
+            return new ResponseEntity<>(candidato, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Filtrar o candidato por Idioma enviando no header
+     * @param language Lingua escolhida pelo usuario para filtrar
+     * @return
+     */
+    @RequestMapping(value = "/candidato/filter/languages/{language}", method = RequestMethod.GET)
+    public ResponseEntity<List<Candidato>> FilterLanguage(@PathVariable(value = "language") String language) {
+        List<Candidato> candidato = candidatoRepository.filterLanguages(language);
+        if (candidato.size() > 0)
+            return new ResponseEntity<>(candidato, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Filtrar o candidato por Idioma e o Nivel do Idioma enviando no header
+     * @param language Lingua escolhida pelo usuario para filtrar
+     * @param level Nivel escolhida pelo usuario para filtrar
+     * @return
+     */
+    @RequestMapping(value = "/candidato/filter/languages/{language}/{level}", method = RequestMethod.GET)
+    public ResponseEntity<List<Candidato>> FilterLanguageWithLevel(@PathVariable(value = "language") String language, @PathVariable(value = "level") String level) {
+        List<Candidato> candidato = candidatoRepository.filterLanguages(language);
+        List<Candidato> list_return = new ArrayList<>();
+        for (Candidato languageLevel : candidato)
+            if (languageLevel.getLanguagesskill_id().stream().toList().get(0).getLevel().getLevelName().equals(level))
+                list_return.add(languageLevel);
+
+        if (list_return.size() > 0)
+            return new ResponseEntity<>(list_return, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
 		  
--- ProjetoGestaoRH/src/main/java/br/com/taking/ProjetoGestaoRH/repository/CandidatoRepository.java
+++ ProjetoGestaoRH/src/main/java/br/com/taking/ProjetoGestaoRH/repository/CandidatoRepository.java
@@ -9,23 +9,30 @@ import br.com.taking.ProjetoGestaoRH.model.Candidato;
 
 
 public interface CandidatoRepository extends JpaRepository<Candidato, Integer>{
-	
-	
-	
-	@Query("select c from Candidato c where c.identificationDocument= :identificationDocument" )
-	public List<Candidato> findAll(String identificationDocument);
-	
-	   //Candidato findByIdentificationDocument(String identification);
-	   
-       // nome da entidade e atributo  // c.id(do meu bd seja igual id dentro parametro abaixo
-	//@Query("select c from Candidato c where c.id = :id or c.nome = :firstName or  c.numero = :identificationDocument")
-	//List<FiltroCandidato> filter (int id, String firstName, String identificationDocument);
-	   
-	   
+
+
+    @Query("select c from Candidato c where c.identificationDocument= :identificationDocument")
+    public List<Candidato> findAll(String identificationDocument);
+
+    @Query("select c from Candidato c where DATE_FORMAT(CURDATE(), '%Y') - DATE_FORMAT(c.birthdate, '%Y') >= :age1 " +
+            "and DATE_FORMAT(CURDATE(), '%Y') - DATE_FORMAT(c.birthdate, '%Y') <= :age2 " +
+            "and DATE_FORMAT(CURDATE(), '%Y') - DATE_FORMAT(c.birthdate, '%Y') <= 35 " +
+            "and DATE_FORMAT(CURDATE(), '%Y') - DATE_FORMAT(c.birthdate, '%Y') >= 18")
+    List<Candidato> filterAges(int age1, int age2);
+
+
+    @Query("select c from Candidato c where DATE_FORMAT(CURDATE(), '%Y') - DATE_FORMAT(c.birthdate, '%Y') = :age")
+    List<Candidato> filterDate(int age);
+
+    @Query("select c, ls, l, lv from Candidato c, Languages_skill ls, Languages l, Level lv WHERE c.candidato_id = ls.candidato_id AND ls.language.language = l.language AND lv.level = ls.language.language AND l.languageName = :language" )
+    List<Candidato> filterLanguages(String language);
+
+    //Candidato findByIdentificationDocument(String identification);
+    // nome da entidade e atributo  // c.id(do meu bd seja igual id dentro parametro abaixo
+    //@Query("select c from Candidato c where c.id = :id or c.nome = :firstName or  c.numero = :identificationDocument")
+    //List<FiltroCandidato> filter (int id, String firstName, String identificationDocument);
+
     // nome da entidade e atributo
-     //@Query("select c from Candidato c where c.id = :id or c.nome = :nome or  c.numero = :numero")
+    //@Query("select c from Candidato c where c.id = :id or c.nome = :nome or  c.numero = :numero")
     //  List<Candidato> filter (int id, String nome, String numero);
-	
-
-
 }
